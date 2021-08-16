8ec078d0d148aa099d785d5cce36e9da8139eb8d
Mph96 <matthew.hernandez@hcl.com>
2021-08-16 12:05:42 -0700

    Completed Project

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+print("Welcome to the Band Name Generator!")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+city_name = input("What's the name of the city you grew up in?\n")
 
+# print(city_name)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pet_name = input("What's the name of your pet?\n")
 
+# print(pet_name)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Your band name could be: " + city_name + " " + pet_name)
